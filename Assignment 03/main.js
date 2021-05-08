474209897042600fdc129532ee44b6dcd1f23494
thalisgold <thalis97@hotmail.de>
2021-05-08 13:22:27 +0200

    Finished requirements

--- Assignment 03/main.js
+++ Assignment 03/main.js
@@ -27,9 +27,9 @@ function loadWeatherAtPosition(latitude, longitude){
     //xhttp.onerror = errorcallback;
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200){
-            let res = JSON.parse(this.responseText)
-            console.log(res);
-            //showWeatherAtPosition(res);
+            let weatherData = JSON.parse(this.responseText)
+            console.log(weatherData);
+            showWeatherAtPosition(weatherData);
         } 
     }
     xhttp.open("GET", `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`, true);
@@ -37,4 +37,34 @@ function loadWeatherAtPosition(latitude, longitude){
 
 }
 
-//showWeatherAtPosition(weatherData){}
+function showWeatherAtPosition(weatherData){
+    //displaying the date
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    let currentDt = new Date(weatherData.current.dt * 1000).toLocaleDateString("EN-EN", options);
+    document.getElementById("currentDate").innerHTML = "Date: " + currentDt;
+
+    //displaying the current temperature
+    console.log(weatherData.current.temp);
+    document.getElementById("currentTemp").innerHTML = "Current temperature: " + weatherData.current.temp + " [celcius]";
+
+    //displaying "feels-like" temperature
+    document.getElementById("currentTempFeelsLike").innerHTML = "Feels-like temperature: " + weatherData.current.feels_like + " [celsius]";
+
+    //displaying the current wind
+    document.getElementById("currentWind").innerHTML = "Wind speed/direction: " + weatherData.current.wind_speed + " [m/s]/" + weatherData.current.wind_deg + " [degrees]";
+
+    //displaying the current humidity 
+    document.getElementById("currentHumidity").innerHTML = "Humidity: " + weatherData.current.humidity + "[%]";
+
+    //displaying the current clouds
+    document.getElementById("currentClouds").innerHTML = "Cloudiness: " + weatherData.current.clouds + "[%]";
+
+    //displaying current rain
+    //document.getElementById("currentRain").innerHTML = "Rainfall in the last 60 minutes: " + weatherData.current.rain.1h + "[mm]";
+
+    //displaying current weather symbol
+    let src = `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`
+    img = document.createElement('img');
+    img.src = src;
+    document.body.appendChild(img);
+}
