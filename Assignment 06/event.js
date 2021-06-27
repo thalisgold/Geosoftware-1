09ea3a098bd3c08060a040ba5c913e3de5a7d58c
thalisgold <thalis97@hotmail.de>
2021-06-27 11:47:15 +0200

    Reset to default

--- Assignment 06/event.js
+++
@@ -1,7 +0,0 @@
-const EventEmitter = require('events')
-class MyEmitter extends EventEmitter {} // new class inherits from EventEmittler
-const myEmitter = new MyEmitter() // create a new instance of MyEmitter
-myEmitter.on('event', () => { // what happens when the event is fired
- console.log('an event occurred!')
-})
-myEmitter.emit('event') // fire the event
--- Assignment 06/livedemo.js
+++
@@ -1,4 +0,0 @@
-console.log("Hello World")
-
-//console.dir(process.env)
-console.dir(process.argv)
